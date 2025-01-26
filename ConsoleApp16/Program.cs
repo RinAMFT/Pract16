55d7d6d0326987e3eb984a315be260b611f937cc
RinAMFT <mucurkaarina@gmail.com>
2025-01-26 22:01:40 +0200

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 16; 
+            for (int i = 0; i < size; i++) 
+            {
+                for (int j = 0; j < size; j++) 
+                {
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(); 
+            }
+           Console.ReadKey();
+        }
+    }
+}
