1b0c7827a62867094df4e884f7eb4b888acf0bd8
RinAMFT <mucurkaarina@gmail.com>
2025-01-26 21:49:20 +0200

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++) 
+            {
+                for (int j = 0; j < 7; j++) 
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                if (i < 3) 
+                {
+                    Console.WriteLine();
+                }
+            }
+          Console.ReadKey();
+        }
+    }
+}
